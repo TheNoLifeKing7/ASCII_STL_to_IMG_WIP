f07c0d2f916b38e9ed06e5a9aa7d2f4520096a82
TheNoLifeKing7 <52138047+TheNoLifeKing7@users.noreply.github.com>
2021-04-13 10:31:25 -0500

    Just copied my whole folder of project in.

---
+++ ASCII_STL_to_JPG/ASCII_STL_to_JPG/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ASCII_STL_to_JPG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStJ = new System.Windows.Forms.Button();
+            this.btnJtS = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStJ
+            // 
+            this.btnStJ.Location = new System.Drawing.Point(66, 156);
+            this.btnStJ.Name = "btnStJ";
+            this.btnStJ.Size = new System.Drawing.Size(75, 23);
+            this.btnStJ.TabIndex = 0;
+            this.btnStJ.Text = "STL to JPG";
+            this.btnStJ.UseVisualStyleBackColor = true;
+            // 
+            // btnJtS
+            // 
+            this.btnJtS.Location = new System.Drawing.Point(329, 156);
+            this.btnJtS.Name = "btnJtS";
+            this.btnJtS.Size = new System.Drawing.Size(75, 23);
+            this.btnJtS.TabIndex = 1;
+            this.btnJtS.Text = "JPG to STL";
+            this.btnJtS.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 499);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 499);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnJtS);
+            this.Controls.Add(this.btnStJ);
+            this.Name = "Form1";
+            this.Text = "ASCII STL to JPG";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStJ;
+        private System.Windows.Forms.Button btnJtS;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ASCII_STL_to_JPG/ASCII_STL_to_JPG/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ASCII_STL_to_JPG
+{
+    public partial class Form1 : Form
+    {
+        const string strFile = @"C:\Users\bzend\OneDrive\Desktop\FMDA_F17_ASCII.stl";
+        const string strSave = @"C:\Users\bzend\OneDrive\Desktop\tests\test.bmp";
+        public Form1()
+        {
+            InitializeComponent();
+            string strIn = File.ReadAllText(strFile);
+            double dblLength = strIn.Length;
+            double dblSqrt = Math.Sqrt(dblLength);
+            if (dblSqrt != (int)dblSqrt)
+            {
+                dblSqrt = (int)dblSqrt;
+                while(dblLength % dblSqrt != 0)
+                    dblSqrt++;
+
+            }
+            int h = (int)dblSqrt;
+            int w = (int)dblLength / (int)dblSqrt ;
+            Bitmap bmpRand = new Bitmap(w,h);
+            int intDex = 0;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    int a = 200;
+                    int rgb = (byte)strIn[intDex];
+                    bmpRand.SetPixel(x, y, Color.FromArgb(a, rgb, rgb, rgb));
+                    intDex++;
+                }
+            }
+            pictureBox1.Image = bmpRand;
+            pictureBox1.Image.Save(strSave, ImageFormat.Bmp);
+        }
+    }
+}
